e9882e67c8ab7b522ff4c0ea82aa126a5ce86380
Abraham Markus <abrahammarkus222@gmail.com>
2026-08-21 01:45:51 +0100

    Added Product Schema

--- backend/model/ProductModel.js
+++ backend/model/ProductModel.js
@@ -6,3 +6,20 @@ name:{tpye:String, required:true, trim:true},
  rating:{type:Number,required:true},
 comment:{type:String,required:true,trim:true},
 user:{type:objectId,ref:"User"}},{timestamps:true})
+
+
+const productSchema = mongoose.Schema({
+    name:{type:String,required:true,trim:true},
+    image:{type:String,required:true},
+    brand:{type:String, required:true,trim:true},
+    quantity:{type:Number,required:true},
+    category:{type:objectId,ref:"Category",required:true},
+    description:{type:String,required:true,trim:true},
+    review:[reviewSchema],
+    rating:{type:Number,required:true,default:0},
+    numReviews:{type:Number,required:true,default:0},
+    price:{type:Number,required:true,default:0},
+    countInStock:{type:Number,required:true,default:0}
+},{timestamps:true})
+
+export default Product = mongoose.model("Product",productSchema)
