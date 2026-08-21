604bd29d2f1e4b1b9ec4882365198becb7c2cbdb
Abraham Markus <abrahammarkus222@gmail.com>
2026-08-21 01:48:49 +0100

    Updated Product Schema

--- backend/model/ProductModel.js
+++ backend/model/ProductModel.js
@@ -2,20 +2,19 @@ import mongoose from "mongoose";
 
 const objectId = mongoose.Schema.Types.ObjectId
 const reviewSchema = mongoose.Schema({
-name:{tpye:String, required:true, trim:true},
+name:{type:String, required:true, trim:true},
  rating:{type:Number,required:true},
 comment:{type:String,required:true,trim:true},
-user:{type:objectId,ref:"User"}},{timestamps:true})
+user:{type:objectId,ref:"User",required:true}},{timestamps:true})
 
 
 const productSchema = mongoose.Schema({
     name:{type:String,required:true,trim:true},
     image:{type:String,required:true},
     brand:{type:String, required:true,trim:true},
-    quantity:{type:Number,required:true},
     category:{type:objectId,ref:"Category",required:true},
     description:{type:String,required:true,trim:true},
-    review:[reviewSchema],
+    reviews:[reviewSchema],
     rating:{type:Number,required:true,default:0},
     numReviews:{type:Number,required:true,default:0},
     price:{type:Number,required:true,default:0},
