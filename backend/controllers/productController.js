12c61328678462024bdfd8796111b283cfe5e2dd
Abraham Markus <abrahammarkus222@gmail.com>
2026-08-23 17:33:14 +0100

    Added addReview controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,7 +38,87 @@ export const fetchProducts = async(req,res) =>{
         res.status(200).json({success:true, products})
 
     } catch (error) {
-          console.error("ADD PRODUCT ERROR:", error);
+          console.error("fetch PRODUCT ERROR:", error);
+        return res.status(500).json({success:false,message:"Server error"})
+    }
+}
+export const fetchTopProducts = async (req,res) => {
+
+    try {
+        const topProducts = await Product.find({rating: -1}).limit(5)
+
+        res.status(200).json({success:true, topProducts})
+        
+    } catch (error) {
+          console.error("FetchTopProducts PRODUCT ERROR:", error);
+        return res.status(500).json({success:false,message:"Server error"})
+    }
+}
+
+export const addReview = async (req,res) => {
+
+    try {
+        const {rating,comment} = req.body
+
+        const product = await Product.findById(req.params.id)
+
+        if(product){
+            const alreadyReviewed = await product.reviews.find((r) => r.user.toString() === req.user._id.toString())
+
+            if(alreadyReviewed){
+                return res.status(400).json({success:false, message:"Already reviewed..."})
+            }
+
+            const review = {
+                name: req.user.name,
+                rating:Number(rating),
+                comment,
+                user: req.user._id
+            }
+
+             product.reviews.push(review)
+             product.numReviews = product.reviews.length
+
+        }
+        await product.save()
+        res.status(200).json({success:true,message:"Review Added successfully..."})
+    } catch (error) {
+        console.error("Add Review PRODUCT ERROR:", error);
+        return res.status(500).json({success:false,message:"Server error"})
+    }
+}
+export const fetchProductById = async (req,res) => {
+
+    try {
+        const product = await Product.findById(req.params.id)
+
+        if(!product){
+            return res.status(404).json({success:false, message:"Product not found!"})
+        }
+
+        res.status(200).json({success:true,product})
+        
+    } catch (error) {
+         console.error("find PRODUCT ERROR:", error);
+        return res.status(500).json({success:false,message:"Server error"})
+    }
+}
+export const updateProduct = async (req,res) => {
+//TODO
+}
+export const deleteProduct = async (req,res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id)
+
+        if(!product){
+            return res.status(404).json({success:false, message:"Product not found"})
+        }
+
+        return res.status(200).json({success:true,message:"Product deleted successfully..."
+        })
+        
+    } catch (error) {
+         console.error("find PRODUCT ERROR:", error);
         return res.status(500).json({success:false,message:"Server error"})
     }
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ import expressFormidable from "express-formidable";
 //Admin
 import { authenticated,authorisedAdmin } from "../middlewares/auth.js"
 //Product controller
-import { addProduct, fetchProducts } from "../controllers/productController.js"
+import { addProduct, fetchProductById, fetchProducts, fetchTopProducts,updateProduct,deleteProduct, addReview } from "../controllers/productController.js"
 const router = express.Router()
 
 router.route("/").post(expressFormidable(),authenticated,authorisedAdmin,addProduct).get(fetchProducts)
-
+router.get("/top",fetchTopProducts)
+router.post("/:id/review",authenticated,addReview)
+router.route("/:id").get(fetchProductById).put(authenticated,authorisedAdmin,updateProduct).delete(authenticated,authorisedAdmin,deleteProduct)
 export default router
