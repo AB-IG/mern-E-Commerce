a140756cfcd3df1eba6ecee7a5d5265deca2effa
Abraham Markus <abrahammarkus222@gmail.com>
2026-08-31 15:48:06 +0100

    Order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,17 @@
 import express from "express"
+import { authenticated,authorisedAdmin } from "../middlewares/auth.js" 
+import {
+    ceateOrder,getAllOrders,findOrderbyId,
+    countTotalOrders,calculateTotalSales,
+    markOrderAsPaid,markOrderAsDelivered} from "../controllers/orderController.js"
 
 const router = express.Router()
 
-
+router.route("/create").post(authenticated, ceateOrder)
+router.route("/").get(authenticated, authorisedAdmin, getAllOrders)
+router.route("/total-orders").get(authenticated,authorisedAdmin,countTotalOrders)
+router.route("/total-sales").get(authenticated,authorisedAdmin,calculateTotalSales)
+router.route("/paid").get(authenticated,authorisedAdmin,markOrderAsPaid)
+router.route("/delivered").post(authenticated,authorisedAdmin,markOrderAsDelivered)
+router.route("/:id").get(authenticated,authorisedAdmin,getUserOrders).get(authenticated,findOrderbyId)
 export default router
